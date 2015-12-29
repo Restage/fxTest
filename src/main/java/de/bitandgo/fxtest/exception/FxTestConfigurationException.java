9692003f4053cc4b306be45c4708f24c89cc9534
Richard Kaufmann <richard.waehner@gmx.de>
2015-12-29 18:08:00 +0100

    [fxt-4] new Exception class, which points to a FxTest configuration error

---
+++ src/main/java/de/bitandgo/fxtest/exception/FxTestConfigurationException.java
@@ -0,0 +1,11 @@
+package de.bitandgo.fxtest.exception;
+
+/**
+ * Exception class that indicates a misconfiguration of a FxTest class.
+ *
+ * @author Kaufmann, Richard
+ * @since 0.1.0
+ */
+public class FxTestConfigurationException extends Exception {
+  // nothing do be done here yet
+}
