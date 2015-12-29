49c77be1891ccbac8e185a9132424d7fc03a3286
Richard Kaufmann <richard.waehner@gmx.de>
2015-12-29 16:17:10 +0100

    [fxt-4] renaming of the FxTestNode annotation, because the name indicates it can be used only for the test of javafx.scene.Node elements, but it should also be used for Scenes and Stages later on

--- src/main/java/de/bitandgo/fxtest/annotation/FxTestObject.java
+++ src/main/java/de/bitandgo/fxtest/annotation/FxTestObject.java
@@ -6,13 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a field as {@link javafx.scene.Node} under test. Exactly one field per FxTest class must me marked as FxTestNode.
+ * Marks one and only one field of the test class as the object to test. Only {@link javafx.scene.Node Nodes} (and subclasses),
+ * {@link javafx.scene.Scene Scenes} and {@link javafx.stage.Stage Stages} are valid class types.
  *
  * @author Kaufmann, Richard
  * @since 0.1.0
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface FxTestNode {
-  // no code needed yet
+public @interface FxTestObject {
+  // nothing do be done here yet
 }
