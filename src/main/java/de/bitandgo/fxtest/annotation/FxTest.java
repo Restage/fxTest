72184501ce4b8bf0ae300dadfceeb57caad7f3ce
Richard Kaufmann <richard.waehner@gmx.de>
2015-12-29 15:42:06 +0100

    [fxt-1] changes to Javadoc and unneccessary keyword public in interface mehtod was removed

--- src/main/java/de/bitandgo/fxtest/annotation/FxTest.java
+++ src/main/java/de/bitandgo/fxtest/annotation/FxTest.java
@@ -5,14 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a test method as FxTest. Only methods with this annotation are executed by the FxTest framework. All other methods are ignored.
+ *
+ * @author Kaufmann, Richard
+ * @since 0.1.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface FxTest {
   
   /**
-   * Is this test enabled
-   
+   * Value to explicitly enable or disable FxTests. If this value is not present, the test is implicitly enabled.
+   *
    * @return {@code true} if the test should be executed, otherwise {@code false}
    */
-  public boolean enabled() default true;
+  boolean enabled() default true;
 }
